b9366c06910dc9286d24ea80ab10928436dc382c
Serhii Kuzin <serhii.kuzin@pitchbook.com>
2023-04-02 13:44:22 +0300

    init commit

---
+++ rest-api/src/main/java/com/integration/rest/HomeController.java
@@ -0,0 +1,16 @@
+package com.integration.rest;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping
+    public String getGreetings(@AuthenticationPrincipal OAuth2User principal) {
+        return principal.getAttribute("name");
+    }
+}
---
+++ src/main/java/com/kuzin/integrationtesting/IntegrationTestingApplication.java
@@ -0,0 +1,18 @@
+package com.kuzin.integrationtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages =
+		{"com.integration.domain",
+		"com.integration.rest",
+		"com.integration.persistence",
+		"com.kuzin.integrationtesting"}
+)
+public class IntegrationTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationTestingApplication.class, args);
+	}
+
+}
