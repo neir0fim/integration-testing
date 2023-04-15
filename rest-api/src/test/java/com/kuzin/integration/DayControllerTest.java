3de4a83444cfe20f384d5992d11c8e6091c7102f
Serhii Kuzin <serhii.kuzin@pitchbook.com>
2023-04-15 20:52:42 +0300

    added README.md

--- rest-api/src/test/java/com/kuzin/integration/DayControllerTest.java
+++ rest-api/src/test/java/com/kuzin/integration/DayControllerTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,9 +28,17 @@ class DayControllerTest {
     private DayService dayService;
 
     @Test
-    void shouldReturnListOfDays() throws Exception {
+    void shouldThrowUnauthorized() throws Exception {
         mockMvc.perform(get("/days"))
                 .andExpect(status()
                         .isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void shouldReturnListOfDays() throws Exception {
+        mockMvc.perform(get("/days"))
+                .andExpect(status()
+                        .isOk());
     }
 }
