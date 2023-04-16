7f666807c123fe285b85d9de992ecd9ace57a973
Serhii Kuzin <serhii.kuzin@pitchbook.com>
2023-04-16 12:39:40 +0300

    fixed error during run test with coverage

--- rest-api/src/test/java/com/kuzin/integration/DayControllerTest.java
+++ rest-api/src/test/java/com/kuzin/integration/DayControllerTest.java
@@ -1,22 +1,27 @@
 package com.kuzin.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kuzin.integration.dto.DayDto;
 import com.kuzin.integration.services.DayService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(controllers = DayController.class)
-@ContextConfiguration(classes = {Config.class})
+@ContextConfiguration(classes = {TestApplication.class})
 class DayControllerTest {
     @Autowired
     protected ObjectMapper mapper;
@@ -37,8 +42,12 @@ class DayControllerTest {
     @Test
     @WithMockUser
     void shouldReturnListOfDays() throws Exception {
-        mockMvc.perform(get("/days"))
-                .andExpect(status()
-                        .isOk());
+        var testDay = new DayDto(1, "MN", "");
+        when(dayService.getDays()).thenReturn(List.of(testDay));
+
+        mockMvc.perform(get("/days").accept(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(content().json(mapper.writeValueAsString(List.of(testDay))))
+                .andExpect(status().isOk());
     }
 }
