2e3cc70306c06d2c992a4a084bc4d7b782975405
Serhii Kuzin <serhii.kuzin@pitchbook.com>
2023-04-17 14:47:39 +0300

    added basic auth

--- rest-api/src/main/java/com/kuzin/integration/config/SecurityConfig.java
+++ rest-api/src/main/java/com/kuzin/integration/config/SecurityConfig.java
@@ -1,22 +1,35 @@
 package com.kuzin.integration.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password(new BCryptPasswordEncoder().encode("admin"))
+                .authorities("ROLE_ADMIN");
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests()
                 .requestMatchers("/")
                 .permitAll()
+                .requestMatchers("/exercises").hasRole("ADMIN")
                 .anyRequest().authenticated()
-                .and().oauth2ResourceServer()
-                .jwt().jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter()).and()
+                .and()
+                .httpBasic()
                 .and().cors()
                 .and().csrf().disable()
                 .build();
